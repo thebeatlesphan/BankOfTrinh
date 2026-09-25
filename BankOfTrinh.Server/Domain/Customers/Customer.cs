048eb47a79db73c53c95583fb2d17d3cfb35fc9e
= <aphanit@protonmail.com>
2026-09-24 21:44:52 -0400

    Implement Customer model

---
+++ BankOfTrinh.Server/Domain/Customers/Customer.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace BankOfTrinh.Server.Domain.Customers;
+
+public sealed class Customer
+{
+    public Guid Id { get; private set; }
+
+    public string FirstName { get; private set; } = string.Empty
+    public string LastName { get; private set; } = string.Empty
+    public string Email { get; private set; } = string.Empty
+
+
+    private Customer() 
+    {
+        // Required by EF Core
+    }
+
+    public Customer(string firstName, string lastName, string email)
+    {
+        Id = Guid.NewGuid();
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+    }
+
+}
